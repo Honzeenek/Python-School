770246d332dfbc8ae1620e5cf0249c290c3687c4
Jan Palenik <jan.palenik@icloud.com>
2025-05-16 09:34:19 +0200

    Add initial implementation of probability calculation for dice rolls

---
+++ cv.19/pravdepodobnostvyhry.py
@@ -0,0 +1,11 @@
+import random
+
+def stesti(n):
+    pocitadlo = 0
+    for i in range(n):
+        cislo = random.randint(1, 6)
+        if cislo == 6:
+            pocitadlo += 1
+    print(f'Pravdepodobnost vyhry pri {n} hodech: {pocitadlo / n}')
+
+stesti(10)

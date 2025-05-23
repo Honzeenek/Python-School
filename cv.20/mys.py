96271e284ee38b7dc733f87f67bb9d86b76ed012
Jan Palenik <jan.palenik@icloud.com>
2025-05-23 09:03:17 +0200

    Refactor klik function to improve readability and add comments for clarity

--- cv.20/mys.py
+++ cv.20/mys.py
@@ -3,5 +3,20 @@ import tkinter
 canvas = tkinter.Canvas()
 canvas.pack()
 
-def klik(mys):  
-    canvas.bind('<B1-Motion>', klik)
+def klik(mys):
+    width = canvas.winfo_width()
+    left_zone = width / 3
+    right_zone = width * 2 / 3
+    
+    if mys.x < left_zone:
+        color = 'red'
+    elif mys.x < right_zone:
+        color = 'yellow'
+    else:
+        color = 'green'
+        
+    canvas.create_oval(mys.x, mys.y, mys.x + 10, mys.y + 10, fill=color)
+
+canvas.bind('<B1-Motion>', klik)
+canvas.mainloop()
+
